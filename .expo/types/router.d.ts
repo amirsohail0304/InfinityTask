418027837905e25a4382338abd01496791609b38
abidhussain173 <62146615+abidhussain173@users.noreply.github.com>
2024-09-21 00:51:40 +0500

    router

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/PreviousNextButtons` | `/(tabs)/aboutRecipe` | `/(tabs)/coffee` | `/(tabs)/home` | `/(tabs)/intro` | `/(tabs)/machine` | `/(tabs)/modalComponent` | `/(tabs)/porta` | `/(tabs)/recipe` | `/(tabs)/recipeTimer` | `/(tabs)/recipeVideo` | `/(tabs)/signup` | `/(tabs)/sliderCard` | `/(tabs)/sliderComponent` | `/(tabs)/welcome` | `/PreviousNextButtons` | `/_sitemap` | `/aboutRecipe` | `/coffee` | `/home` | `/intro` | `/machine` | `/modalComponent` | `/porta` | `/recipe` | `/recipeTimer` | `/recipeVideo` | `/signup` | `/sliderCard` | `/sliderComponent` | `/welcome`;
+      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/PreviousNextButtons` | `/(tabs)/aboutRecipe` | `/(tabs)/coffee` | `/(tabs)/home` | `/(tabs)/intro` | `/(tabs)/machine` | `/(tabs)/modalComponent` | `/(tabs)/porta` | `/(tabs)/recipe` | `/(tabs)/recipeTimer` | `/(tabs)/recipeVideo` | `/(tabs)/signup` | `/(tabs)/sliderCard` | `/(tabs)/sliderComponent` | `/(tabs)/welcome` | `/(tabs)\recipeTimer` | `/PreviousNextButtons` | `/_sitemap` | `/aboutRecipe` | `/coffee` | `/home` | `/intro` | `/machine` | `/modalComponent` | `/porta` | `/recipe` | `/recipeTimer` | `/recipeVideo` | `/signup` | `/sliderCard` | `/sliderComponent` | `/welcome`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
